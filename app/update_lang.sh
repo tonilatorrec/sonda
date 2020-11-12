1f7d33579f6ac2995c639f6df7c1a1670fa302e8
Toni Latorre <tonilatorre.c@gmail.com>
2020-11-12 20:15:41 +0100

    ..

--- app/update_lang.sh
+++
@@ -1,6 +0,0 @@
-xgettext -d base -o locales/base.pot weather.py
-for lang in ca es en
-do
-	msgmerge --update locales/"${lang}"/LC_MESSAGES/base.po locales/base.pot
-	msgfmt -o locales/"${lang}"/LC_MESSAGES/base.mo locales/"${lang}"/LC_MESSAGES/base
-done
